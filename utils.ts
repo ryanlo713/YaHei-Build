384f3647b8ecaa46530a52be056ae6b1a04b750b
hingbong <hingbonglo@gmail.com>
2024-10-08 09:47:49 +0800

    update path

--- utils.ts
+++ utils.ts
@@ -194,4 +194,4 @@ export async function unzip(zipFile: string, dest: string): Promise<string | fal
     return decompress(zipFile, dest)
 }
 
-export const toWinFontsToolPath =`tools/toWinFonts/toWinFonts-main/main/files/weiwei.py`
+export const toWinFontsToolPath =`tools/toWinFonts/WeiFonts-main/files/weiwei.py`
